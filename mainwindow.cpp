7f97117082484523f28f2679037c864f274a254d
coucou1121 <sebastien.blessemaille@heig-vd.ch>
2017-04-02 19:29:37 +0200

    seconde test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//test for commit seconde time
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
